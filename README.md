e1ec8051237cbfd3d06988e160d356e6a8060dc2
yap cheah shen <yapcheahshen@gmail.com>
2024-03-06 22:57:26 +0800

    first release

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mazu
 和媽祖作伙讀經
+
+
+傳妙 天上聖母經
+https://www.youtube.com/watch?v=EStYt20dqOU 台
+https://www.youtube.com/watch?v=aXhi9O_z7bw 國
+
+天上聖母三字經
+http://www.666666.url.tw/16/16.htm
---
+++ src/appstore.js
@@ -0,0 +1,18 @@
+export * from 'accelon23/src/store.js';
+import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
+
+_ACC23.CacheName='v1::mazu',
+_ACC23.AppVer='24.3.6';
+_ACC23.AppName='mazu'
+_ACC23.AppTitle='媽祖'
+_ACC23.allptks=['mazu','mazu-bh'];
+_ACC23.selectedptks=['mazu','mazu-bh']
+_ACC23.activeptk='mazu'
+_ACC23.ptkinfo={
+    'mazu':"文言",
+    'mazu-bh':"白話",
+}
+_ACC23.favorites=[];
+_ACC23.MAXFAVORITE=100;
+_ACC23.ITEMPERPAGE=5;
+export const ACC23 = _ACC23;
